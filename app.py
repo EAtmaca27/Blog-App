3e33cd0b75617fb1623a152d3d8ac799ea9a16a6
EAtmaca27 <enesatmaca27@googlemail.com>
2026-05-07 16:52:02 +0200

    Added '/add' route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,29 @@ def index():
         blog_posts = json.load(f)
 
     return render_template('index.html', blog_posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        content = request.form['content']
+
+        with open('data/posts.json', 'r') as f:
+            blog_posts = json.load(f)
+
+        new_post = {
+            'id': len(blog_posts) + 1,
+            'title': title,
+            'author': author,
+            'content': content
+            }
+        blog_posts.append(new_post)
+        with open('data/posts.json', 'w') as f:
+            json.dump(blog_posts, f)
+
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
