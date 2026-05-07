df7475ebfc232df12e8fbcba276d32760b3ecf18
EAtmaca27 <enesatmaca27@googlemail.com>
2026-05-07 18:12:44 +0200

    Updated - added 'update' route --> fixed some redundancy and PEP-8 conformity

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+def fetch_post_by_id(blog_posts, post_id):
+    for post in blog_posts:
+        if post['id'] == post_id:
+            return post
+    return None
+
+
 @app.route('/')
 def index():
     with open('data/posts.json', 'r') as f:
         blog_posts = json.load(f)
 
     return render_template('index.html', blog_posts=blog_posts)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -22,17 +30,18 @@ def add():
             blog_posts = json.load(f)
 
         new_post = {
-            'id': len(blog_posts) + 1,
+            'id': max(post['id'] for post in blog_posts) + 1 if blog_posts else 1,
             'title': title,
             'author': author,
             'content': content
-            }
+        }
         blog_posts.append(new_post)
         with open('data/posts.json', 'w') as f:
             json.dump(blog_posts, f)
 
         return redirect(url_for('index'))
     return render_template('add.html')
+
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
@@ -45,6 +54,39 @@ def delete(post_id):
         json.dump(updated_blog_posts, f)
 
     return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    with open('data/posts.json', 'r') as f:
+        blog_posts = json.load(f)
+
+    post = fetch_post_by_id(blog_posts, post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        updated_post = {
+            'id': post['id'],
+            'title': request.form['title'],
+            'author': request.form['author'],
+            'content': request.form['content']
+        }
+
+        for post in blog_posts:
+            if post['id'] == updated_post['id']:
+                post['title'] = updated_post['title']
+                post['author'] = updated_post['author']
+                post['content'] = updated_post['content']
+                break
+
+        with open('data/posts.json', 'w') as f:
+            json.dump(blog_posts, f)
+
+        return redirect(url_for('index'))
+
+    return render_template('update_form.html', post=post)
 
 
 if __name__ == '__main__':
